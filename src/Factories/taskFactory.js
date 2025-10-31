d9bc6ed9d1b173e24fabb4f8c6b25d535281a324
Sergi Juarez <sergijp22@gmail.com>
2025-10-31 13:43:36 +0100

    Updates

--- src/Factories/taskFactory.js
+++
@@ -1,9 +0,0 @@
-import Task from "../Models/task";
-
-export function createBlankTask() {
-  return new Task("", "", "", 0, "", false);
-}
-
-export function createDemoTask() {
-  return new Task("Visit imserez github!", "", 0, "", false);
-}
